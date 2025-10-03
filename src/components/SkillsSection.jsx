3498ffeef01f438248acdfbbeed7f5a2e720a955
Akshay kumar <kakshay75760@gmail.com>
2025-10-03 16:54:32 +0530

    Portfolio

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,3 +1,5 @@
+import { GitBranch } from "lucide-react";
+
 const SkillsSection = () => {
   const skillCategories = [
     {
@@ -35,7 +37,7 @@ const SkillsSection = () => {
     },
     {
       title: "Database & Tools",
-      skills: ["MySQL", "Postman","Swagger"],
+      skills: ["MySQL", "Postman","Swagger" , "Git" , "GitHub" ,],
       icon: (
         <svg className="w-8 h-8 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
@@ -66,6 +68,7 @@ const SkillsSection = () => {
       bgColor: "bg-blue-100 dark:bg-blue-900/30",
       iconBg: "bg-blue-600"
     }
+    ,
   ];
 
   return (
